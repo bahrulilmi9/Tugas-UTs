cd526211aa523353e2f62b2f6d678d383192d4d5
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 20:50:00 +0800

    Mengetahui Nilai Positif atau Negatif

--- No4.cpp
+++ No4.cpp
@@ -6,4 +6,14 @@ int main(){
 	int x;
 	cout << "Masukan Nilai Integer : ";
 	cin >> x;
+	
+	if (x>0){
+		cout << "Nilai positif";
+	}
+	else if (x<0){
+		cout << "Nilai negatif";
+	}
+	else{
+		cout << "Nilai Nol";
+	}
 }
