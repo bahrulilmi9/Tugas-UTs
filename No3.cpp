f61096cc31242789cdb0475277fa67828094c7bd
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 20:43:17 +0800

    Menghitung Gajih dan Jam Kerja Karyawan

---
+++ No3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int gp,jk;
+	int lembur,gt,pajak,gk;
+	
+	cout<<"Perhitungan Gaji Karyawan"<<endl;
+	cout<<"Masukan Gaji Pokok Anda : Rp. ";
+	cin >>gp;
+	cout <<"Masukan Jam Kerja Anda : ";
+	cin >>jk;
+}
