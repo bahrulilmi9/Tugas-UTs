b9a5911bacdf21232a5d8ef3bce5af34e840475a
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 20:54:36 +0800

    Hasil Perhitungan Berat + Tinggi

--- No5.cpp
+++ No5.cpp
@@ -9,4 +9,15 @@ int main(){
 	cin >>berat;
 	cout <<"Masukan Tinggi Badan Anda : ";
 	cin >>tinggi;
+	
+	cout <<"Hasil Perhitungan Anda"<<endl;
+	cout <<"Berat Badan Anda : "<<berat<<endl;
+	cout <<"Tinggi Badan Anda : "<<tinggi<<endl;
+	if (berat < tinggi/2.5){
+		cout <<"Anda Underweight";
+	}else if(tinggi/2.3 < berat){
+		cout <<"Anda Overweight";
+	}else if(tinggi/2.5 <= berat <= tinggi/2.3) {
+		cout <<"Anda Normal";
+	}
 }
