2b89c8a82a6e33f25fb6fe87b0c49485130d26c9
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 20:45:27 +0800

    Menghitung Jam Lembur PPN dan Total Gajih

--- No3.cpp
+++ No3.cpp
@@ -10,4 +10,17 @@ int main(){
 	cin >>gp;
 	cout <<"Masukan Jam Kerja Anda : ";
 	cin >>jk;
+	
+	
+	lembur=jk-40;
+	gt=(lembur*1.5+40)*gp;
+	pajak=gt*0.15;
+	gk=gt-pajak;
+	
+	cout<<endl;
+	cout<<"Gaji Pokok  : Rp."<<gp<<endl;
+	cout<<"Jam Kerja   : "<<jk<<endl;
+	cout<<"Lembur      : "<<lembur<<endl;
+	cout<<"Gaji Sebelum Dipotong Pajak : Rp."<<gt<<endl;
+	cout<<"Total Gaji Anda Adalah : Rp."<<gk;
 }
