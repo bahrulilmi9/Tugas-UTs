6c9571120b02834a84a100e96414814cded1040d
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 20:40:06 +0800

    Menghitung Selisih Absolute, Menentukan Postif dan Negatif

--- No1.cpp
+++ No1.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
+	int x,y,z;
 	
+	cout<<"Menghitung Selisih Absolute"<<endl;
+	cout<<"Masukan Nilai X : ";
+	cin>>x;
+	cout<<"Masukan Nilai Y : ";
+	cin>>y;
+	
+	if (x>y){
+		z=x-y;
+	}
+	else if (y>x){
+		z=y-x;
+	}
+	cout<<"Hasil : "<<z;
 }
