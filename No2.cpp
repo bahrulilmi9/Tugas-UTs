fe44737d34cda1499bed10b2406be3c03543eaff
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 14:32:40 +0800

    Masukkan Inputan dan menghitung

--- No2.cpp
+++ No2.cpp
@@ -7,4 +7,20 @@ int main(int argc, char** argv){
 	cout <<"1.Persegi panjang\n";
 	cout <<"2.Segitiga\n";
 	cout <<"Masukkan Pilihan =";cin >>pilihan;
+	
+	switch(pilihan){
+		case 1:
+		cout<<"Masukkan panjang=";cin>>panjang;
+		cout<<"Masukkan lebar=";cin>>lebar;
+		luas =panjang * lebar;
+		cout<<"luas persegi panjang="<<luas<<"\n";
+		system("pause");
+		break;
+		case 2:
+		cout<<"Masukkan panjang alas tinggi=";cin>>alas;
+		cout<<"Masukkan tinggi segitiga=";cin>>tinggi;
+		luas=0.5*alas*tinggi;
+		cout<<"Luas segitiga="<<luas<<"\n";
+		system("pause");
+	}
 }
