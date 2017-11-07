e4e369dce8a057759cd4d24a261a608c042d1069
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 20:53:19 +0800

    Input Berat dan Tinggi

--- No5.cpp
+++ No5.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
+	double berat, tinggi;
 	
+	cout <<"Perhitungan Berat Badan Ideal"<<endl;
+	cout <<"Masukan Berat Badan Anda : ";
+	cin >>berat;
+	cout <<"Masukan Tinggi Badan Anda : ";
+	cin >>tinggi;
 }
