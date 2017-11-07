15a2e34f0acba07ff8fbd1b1aa7742f80365dcf0
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2017-11-07 14:23:32 +0800

    Panjang Lebar DLL

--- No2.cpp
+++ No2.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+	int panjang, lebar, luas, pilihan, alas, tinggi;
+	
+	cout <<"1.Persegi panjang\n";
+	cout <<"2.Segitiga\n";
+	cout <<"Masukkan Pilihan =";cin >>pilihan;
 }
